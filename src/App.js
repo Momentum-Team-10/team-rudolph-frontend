e81c05ca2a6b981356e51b353fdaa69fc5044ed1
TrentHurlbut <trent.hurlbut@gmail.com>
2021-12-14 11:17:12 -0800

    Added navbar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/NavBar';
 import QuestionCard from './components/QuestionCard.js'
 
 function App() {
   return (
+    <>
+    <NavBar byCreatedAt='Filter Call by Most Recent' byHighestRated='Filter Call by Highest Rated' />
     <QuestionCard questionTitle='Question Title Goes Here' votesCounter='voteCounter' answersCounter='answerCounter' viewsCounter='viewsCounter'></QuestionCard>
+    </>
   );
 }
 
