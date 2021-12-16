39027820ff5b4ecbee38f9e8dae911b60f30a492
James Moore <james816am@gmail.com>
2021-12-16 12:36:24 -0500

    Small bug fixes, GET request working for questions, homepage is populating with list of question cards

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import NavBar from './components/NavBar';
 import QuestionCard from './components/QuestionCard.js'
 import QuestionCardZoom from './components/QuestionCardZoom.js'
 import ResponseCard from './components/ResponseCard.js'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [user, setUser] = useState({})
+  const [questionList, setQuestionList] = useState([])
+
+  useEffect(() => {
+    const questionsUrl = 'https://questions-t10.herokuapp.com/questions/'
+    axios
+      .get(questionsUrl)
+      .then((response) => {
+        console.log(response.data)
+        setQuestionList(response.data)
+      })
+  }, [])
 
   return (
     <>
@@ -16,9 +28,28 @@ function App() {
       userImg="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
       user={user}
       setUser={setUser}/>
-    <QuestionCard questionTitle='Question Title Goes Here' votesCounter='10' answersCounter='2' viewsCounter='204'></QuestionCard>
-    <QuestionCardZoom questionTitle='Question Title Goes Here' questionText='This is the text of the question' attachments='Attachment'></QuestionCardZoom>
-    <ResponseCard responseText='Response text goes here' responseUpvotes={25} responseDownvotes={10} bestAnswer={true}></ResponseCard>
+      {questionList.map((question) => (
+        <QuestionCard
+        questionTitle={question.title}
+        votesCounter={question.votes}
+        answersCounter={question.answers.length}
+        author={question.author}
+        />
+      ))}
+    <QuestionCard
+      questionTitle='Question Title Goes Here'
+      votesCounter='10'
+      answersCounter='2'
+      viewsCounter='204'/>
+    <QuestionCardZoom
+      questionTitle='Question Title Goes Here'
+      questionText='This is the text of the question'
+      attachments='Attachment'/>
+    <ResponseCard
+      responseText='Response text goes here'
+      responseUpvotes={24}
+      responseDownvotes={10}
+      bestAnswer={true}></ResponseCard>
     </>
   );
 }
