655cc72a5dc6771ecaf548ae6b2271caeaf600b1
James Moore <james816am@gmail.com>
2021-12-16 16:38:03 -0500

    Navbar added to question zoomed-in pages. Pretty sure the useState part of that is NOT implemented correctly currently, something to test and look into

--- src/routes/questionZoom.jsx
+++ src/routes/questionZoom.jsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import QuestionCardZoom from '../components/QuestionCardZoom'
 import ResponseCard from '../components/ResponseCard'
+import NavBar from '../components/NavBar'
 
 export default function QuestionZoom() {
     let params = useParams()
     const [questionData, setQuestionData] = useState([])
     const [answerData, setAnswerData] = useState([])
+    const [user, setUser] = useState({})
+    
     
     useEffect(() => {
         const questionUrl = `https://questions-t10.herokuapp.com/questions/${params.questionId}`
@@ -22,6 +25,12 @@ export default function QuestionZoom() {
       }, [params])
     return (
         <>
+        <NavBar
+        byCreatedAt='Filter Call by Most Recent'
+        byHighestRated='Filter Call by Highest Rated'
+        userImg="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png"
+        user={user}
+        setUser={setUser} />
         <QuestionCardZoom
             questionTitle={questionData.title}
             questionText={questionData.body}
