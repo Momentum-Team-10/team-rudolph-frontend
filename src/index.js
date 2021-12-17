36fc943acd883d8548cc472a495e743b816ce096
James Moore <james816am@gmail.com>
2021-12-17 12:52:12 -0500

    Routing fixed, I think...

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import QuestionZoom from "./routes/questionZoom"
 // import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path='/' element={<App />} /> 
+//       <Route path='questions/:questionId' element={<QuestionZoom />}/>
+//     </Routes>
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} /> 
-      <Route path='questions/:questionId' element={<QuestionZoom />}/>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
